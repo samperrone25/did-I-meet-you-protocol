94d55f64c90aea0b79107ba8f127816b0f5fdc47
samperrone25 <68690083+samperrone25@users.noreply.github.com>
2022-04-21 22:44:29 +1000

    Polished Comments

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
-BACKEND_IP = '192.168.88.133' # found with linux: 'ifconfig'
+BACKEND_IP = '192.168.88.135' # found with linux: 'ifconfig', may not be correct initially
 BACKEND_PORT = 55000 # 55000
 NODE_PORT = 38000
 
 ID_TIMER = 15 # meant to be 15s
 BROADCAST_INTERVAL = 3 # meant to be 3s
 
-DBF_TIMER = 30 # meant to be 90s
-QBF_TIMER = 60 # meant to be 9 minutes = 540s
+DBF_TIMER = 90 # meant to be 90s
+QBF_TIMER = 540 # meant to be 9 minutes = 540s
